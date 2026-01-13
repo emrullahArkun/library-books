cbd509d3cafc4378c3cc0e46ba1580b6f6fe2648
emrullaharkun <emrullah.arkun@gmail.com>
2026-01-13 22:42:03 +0100

    -

--- backend/src/main/java/com/example/minilibrary/repository/BookRepository.java
+++ backend/src/main/java/com/example/minilibrary/repository/BookRepository.java
@@ -1,12 +1,8 @@
 package com.example.minilibrary.repository;
 
 import com.example.minilibrary.model.Book;
-import com.example.minilibrary.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
